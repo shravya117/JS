b18af54dc349170cbb214db4b1a4be9e5f1319ce
Shravya K <shravyarao117@gmail.com>
2025-11-28 20:15:04 +0530

    Add files via upload

---
+++ program12.js
@@ -0,0 +1,9 @@
+// Program: Check if number is even or odd
+function checkEvenOdd(num) {
+    if (num % 2 === 0) {
+        return 'Even';
+    } else {
+        return 'Odd';
+    }
+}
+console.log('15 is:', checkEvenOdd(15));
