4d7a121c7236d04367111a3e4639a9d9f67fa866
Shravya K <shravyarao117@gmail.com>
2025-11-28 20:16:22 +0530

    Add files via upload

---
+++ program19.js
@@ -0,0 +1,6 @@
+// Program: Check if string is palindrome
+function isPalindrome(str) {
+    const cleaned = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    return cleaned === cleaned.split('').reverse().join('');
+}
+console.log('Is racecar a palindrome?', isPalindrome('racecar'));
